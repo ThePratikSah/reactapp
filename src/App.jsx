076eba013d676671a47cd36af01105ce298c26c0
Pratik Sah <shivam3448@gmail.com>
2020-06-23 02:05:02 +0530

    Created first react app.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hello World!</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+import Header from "./Header";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <Header />
+    </div>
+  );
+}
+
+export default Footer;
